71d5208e976983394f755fd1b4a34aa99e2cc00c
Michael Craig <michaelcraig@Michaels-MacBook-Pro.local>
2018-12-30 09:59:27 -0500

    first commit

---
+++ src/main/java/service/AccTypeService.java
@@ -0,0 +1,12 @@
+package service;
+
+import dao.AccountTypeDao;
+import dao.Dao;
+import pojos.AccountType;
+
+public class AccTypeService {
+
+	static Dao<AccountType, Integer> accountTypeDao = new AccountTypeDao();
+
+	
+}
